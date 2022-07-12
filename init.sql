b12803c391cabeb0b55da7b5a2693f7c9fda54b7
jedison vieira <jedison.vieira@hotmail.com>
2022-07-12 16:25:21 -0300

    starting project

--- init.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE bankaccount;
-CREATE USER bankaccount WITH ENCRYPTED PASSWORD 'bankaccount';
-GRANT ALL PRIVILEGES ON DATABASE bankaccount TO bankaccount;
--- src/native-test/kotlin/transactionService/TransactionIT.kt
+++
@@ -1,6 +0,0 @@
-package transactionService
-
-import io.quarkus.test.junit.QuarkusIntegrationTest
-
-@QuarkusIntegrationTest
-class TransactionIT : TransactionTest()
