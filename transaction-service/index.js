96261cb7ce54598b86a8674ee0c93976fb51096d
jedison vieira <jedison.vieira@hotmail.com>
2022-07-13 09:18:55 -0300

    initial commit

---
+++ transaction-service/index.js
@@ -0,0 +1,9 @@
+import { MongoClient } from 'mongodb';
+import { } from 'express';
+import { } from 'body-parser';
+import { } from 'amqplib/callback_api';
+import { os } from 'os';
+
+while (true) {
+   console.log(os.name);
+}
