28df1e1d2d8a34d367e42bf8ab2be67781580206
WJunior13 <juninho_carrer@hotmail.com>
2022-07-20 00:30:35 -0300

    ajustes portas e migration

---
+++ migration-money-app/src/main/resources/db/migration/V1__create.sql
@@ -0,0 +1,13 @@
+create table clients (
+    id varchar not null primary key,
+    nome varchar not null,
+    cpf varchar not null unique
+);
+CREATE TABLE balances (
+    id varchar not null primary key,
+    last_transaction_id varchar not null,
+    account_id varchar not null unique,
+    value float8 not null
+);
+
+create index balances_account_id_index on balances (account_id);
